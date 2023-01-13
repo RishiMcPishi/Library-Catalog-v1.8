2dc717351b3d4726d1a147f6bdb27c1644902a68
Rishi J <>
2023-01-13 01:18:12 +0000

    Added emoji's and error specific messages. There is also punctuation in the text now. Other minor changes in the wording of the text.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 ################################
 
 # import libraries
+import os
 import sys
 import time
 import json
@@ -17,7 +18,7 @@ with open(filename) as f:
   books_in = json.load(f)
 
 passwords = {
-  'rishi': 'math81',
+  'rishi': os.getenv("rishi_password"),
   'brody': 'hockey95',
   'francisco': 'soccer34',
 }
@@ -29,60 +30,39 @@ books = {
   '1234': 'Amulet',
 }
 
-# books_in = {
-# 4321 : 'Harry Potter',
-# 1111 : 'Legend',
-# 1234 : 'Amulet',
-# }
-
 # give welcome message
 print(
   '--------------------------------------------------------------------------------',
   "\033[36m")
-print("Welcome to the catalog system!", "\033[0m")
-
-# ask to reset: for developers ONLY
-resetting = input('Would you like to reset? [Y/n] ')
-if resetting == 'y' or resetting == 'Y':
-  print('Processing...')
-  # reset checked out books
-  people = {
-    'rishi': list(),
-    'brody': list(),
-    'francisco': list(),
-  }
-  books_in = {
-    "4321": 'Harry Potter',
-    "1111": 'Legend',
-    "1234": 'Amulet',
-  }
-
-  time.sleep(3)
+print("📚 Welcome to the catalog system! 📚", "\033[0m")
 
 # ask for name + password
 name = input('What is you name? (all lower case) ')
 if name not in people.keys():
-  print('Your name is not verified')
+  print('Your name is not verified.')
   sys.exit()
 
 password = secret_input('What is your password? ')
 if not password == passwords[name]:
-  print('Your password is not verified')
+  # password is incorrect
+  print('Your password is not verified.')
   sys.exit()
 
 print('Processing...')
 time.sleep(3)
 
 # if name and password are correct the user will be let into the system
-print('You have been verified. Welcome Mr.', name.title())
+print(f'You have been verified. Welcome Mr. {name.title()}! 😀')
 
 # ask for which transaction the user would like to do
 print('1 - Borrow')
 print('2 - Return')
-print('3 - Look at the books you have checked out')
+print('3 - Look at the books you have checked out.')
+if name == 'rishi':
+  print('4 - Reset')
 options = input('What transaction would you like to do? ')
 
-if int(options) > 3:
+if int(options) > 3 and not name == 'rishi':
   sys.exit()
 
 print('Processing...')
@@ -97,25 +77,33 @@ if options == '1':
 
   # check if library has book
   if barcode in books_in.keys():
-    print('Your book has been found')
+    print('Your book has been found.')
     # make sure there was no typo in the barcode
     confirm = input(
       f'Just to confirm, you would like to check out {books_in[barcode]}? [Y/n] '
     )
     if confirm == 'Y' or confirm == 'y':
-      print('Ok!')
+      print('Ok! 😀')
     else:
       print('Oh, looks like you made a typo.')
       sys.exit()
   else:
-    print('This book has been checked out or is not in our library')
+    # check for multiple cases of the book not being able to get checked out
+    k = people[name]
+    if barcode in k:
+      print('You have already checked out this book.')
+    elif barcode in books:
+      print('This book has been checked out.')
+    else:
+      print('This book is not in our library.')
     sys.exit()
 
   # check out book
   people[name].append(barcode)
   del books_in[barcode]
   print(
-    'Your book has been checked out in the system, you may take the book now.')
+    'Your book has been checked out in the system, you may take the book now. 😀'
+  )
 elif options == '2':
   """You would like to return"""
   barcode = input('What is the code of the book you want to return? ')
@@ -129,23 +117,36 @@ elif options == '2':
     confirm = input(
       f'Just to confirm, you would like to return {books[barcode]}? [Y/n] ')
     if confirm == 'Y' or confirm == 'y':
-      print('Ok!')
+      print('Ok! 😀')
     else:
       print('Oh, looks like you made a typo.')
       sys.exit()
     del k[i]
     people[name] = k
-    books_in.update({barcode: books[barcode]}) # see sources
-    print('Your book has been successfully returned')
+    books_in.update({barcode: books[barcode]})  # see sources at the bottom
+    print('Your book has been successfully returned. 😀')
   else:
-    print('This book has not been checked out or is not in our library')
+    print('You have not checked out this book.')
     sys.exit()
-else:
+elif options == '3':
   if len(people[name]) > 0:
     for book in people[name]:
       print('• ', books[book])
   else:
-    print('You have no books checked out')
+    print('You have no books checked out.')
+elif options == '4':
+  # ask to reset: for me ONLY
+  print('Done! 😀')
+  # reset checked out books
+  people = {
+    'rishi': list(),
+    'brody': list(),
+    'francisco': list(),
+  }
+  books_in = {"4321": "Harry Potter", "1111": "Legend", "1234": "Amulet"}
+else:
+  # you gave number that doesn't correspond to an option
+  sys.exit()
 
 print(
   '--------------------------------------------------------------------------------'
@@ -159,5 +160,5 @@ filename = 'books_in.json'
 with open(filename, 'w') as f:
   json.dump(books_in, f)
 
-# SOURCES
+# Thanks to these scources that helped me make this project what it is now
 # https://www.w3schools.com/python/ref_dictionary_update.asp
